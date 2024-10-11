63f4bef6efb99508959f252d6ec22328a88822ab
Senbonzakura Kageyoshi <116958866+kuchiki-byakuya@users.noreply.github.com>
2024-10-11 20:46:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
-document.getElementById('notifyBtn').addEventListener('click', function() {
-    console.log("Button clicked"); // Log when button is clicked
+const button = document.querySelector("button")
+button.addEventListener('click', (e)=>{
+	const heading = document.querySelector('h1')
+	heading.innerHTML = "Its me adarsha I am gay !!!"
+	const image = document.querySelector('.pic')
+	image.innerHTML = "<img src = './Downloads/adarsha.jpeg'>"
 
-    if ("Notification" in window) {
-        console.log("Notification API is supported");
+})
 
-        if (Notification.permission === 'granted') {
-            console.log("Permission granted");
-            notify();
-        } else {
-            console.log("Requesting notification permission");
-            Notification.requestPermission().then((permission) => {
-                if (permission === 'granted') {
-                    console.log("Permission granted after request");
-                    notify();
-                } else {
-                    console.log("Permission denied");
-                }
-            });
-        }
-    } else {
-        console.log("Notification not supported");
-    }
-});
 
-function notify() {
-    const notification = new Notification("Hello World", {
-        body: "This is your device notification!",
-        icon: "https://via.placeholder.com/128" // Example icon
-    });
-    console.log("Notification shown:", notification);
-}
